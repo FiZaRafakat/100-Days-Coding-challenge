ffe0f3d80dd0281359e5f6cfa0533a4adf3233d9
FiZaRafakat <164252341+FiZaRafakat@users.noreply.github.com>
2024-06-15 02:45:18 -0700

    Coding Challenge 6

---
+++ Day29/index.ts
@@ -0,0 +1,23 @@
+//*Question 85:* Finding the Position of a Substring: Write a function that locates the first occurrence of the word "code" within any given string and returns its position.
+
+function positionOfCode(sentence : string){
+    return sentence.indexOf("code")
+}
+
+console.log(positionOfCode("Do you want to write code in TypeScript."));
+
+//*Question 86:* Checking for Text Presence: Create a function that checks if the word "JavaScript" is present in a given string. It should return true if found, otherwise false.
+
+function hasJavaScript(sentence:string):boolean{
+    return sentence.includes("JavaScript")
+}
+
+console.log(hasJavaScript("I like to code in JavaScript"));
+console.log(hasJavaScript("I love to write a code in TypeScript."));
+
+//*Question 87:* Extracting a Substring: Demonstrate how to extract the first 10 characters from a string.
+
+function extractSubstring(string : string):string{
+         return string.substring(0,10)
+}
+console.log(extractSubstring("Do you really Enjoy coding in typeScript."));
