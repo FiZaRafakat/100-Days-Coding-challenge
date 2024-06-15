c61d12298481c33a619616d56a0ece5ba0804869
FiZaRafakat <164252341+FiZaRafakat@users.noreply.github.com>
2024-06-15 09:30:59 -0700

    Coding Challenge 6

---
+++ Day30/index.js
@@ -0,0 +1,17 @@
+//*Question 88:* Rounding to the Nearest Integer: Write a function that takes a decimal number as input and returns the number rounded to the nearest integer.
+function roundToInteger(number) {
+    return Math.round(number);
+}
+console.log(roundToInteger(7.0008));
+//*Question 89:* Converting Strings to Numbers: Create a function that takes a string representing a number (like "123") and converts it into an actual number type.
+function convertStrToNum(num) {
+    return parseFloat(num);
+}
+console.log(convertStrToNum("7890"));
+console.log(convertStrToNum("234.342"));
+// *Question 90:* Checking if a Value is NaN: Demonstrate how to check if a variable's value is NaN (Not a Number) and return a boolean result
+function checkNaN(type) {
+    return isNaN(type);
+}
+console.log(checkNaN("Shiza"));
+console.log(checkNaN(123));
