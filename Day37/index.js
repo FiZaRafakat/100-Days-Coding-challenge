64042cc479af20a54988ed316fa74468553fda17
FiZaRafakat <164252341+FiZaRafakat@users.noreply.github.com>
2024-06-19 01:40:03 -0700

    Coding Challenge 6

---
+++ Day37/index.js
@@ -0,0 +1,39 @@
+//*Question 109:* Write an if statement that logs "Good Morning" if the current time is before 12 PM.
+var currentTime = new Date();
+if (currentTime.getHours() > 12) {
+    console.log("Good Morning");
+}
+//*Question 110:* Create a function that assigns a grade (A, B, C, D, F) based on a student's score.
+function grades(score) {
+    if (score >= 90) {
+        return "A";
+    }
+    else if (score >= 80) {
+        return "B";
+    }
+    else if (score >= 70) {
+        return "C";
+    }
+    else if (score >= 60) {
+        return "D";
+    }
+    else {
+        return "F";
+    }
+}
+console.log(grades(89));
+//*Question 111:* Use an if-else-if chain to categorize a person's age group (child, teenager, adult).
+function categorizeAgeGroups(age) {
+    if (age < 13) {
+        return "Child";
+    }
+    else if (age < 19) {
+        return "Teenager";
+    }
+    else {
+        return "Adult";
+    }
+}
+console.log(categorizeAgeGroups(34));
+console.log(categorizeAgeGroups(11));
+console.log(categorizeAgeGroups(18));
