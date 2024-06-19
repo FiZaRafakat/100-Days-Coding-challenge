b79b54156bdc323033cd61089a89c5bc67be1458
FiZaRafakat <164252341+FiZaRafakat@users.noreply.github.com>
2024-06-18 22:18:11 -0700

    Coding Challenge 6

---
+++ Day34/index.js
@@ -0,0 +1,11 @@
+//*Question 100:* Use the JavaScript Math object to find the square root of 144.
+var squareRoot = Math.sqrt(81);
+console.log(squareRoot);
+//*Question 101:* Generate a random integer between 1 and 10.
+function randomInteger() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+console.log(randomInteger());
+//*Question 102:* Calculate and log the absolute difference between the number -5 and 5.
+var difference = Math.abs(-6 - 5);
+console.log(difference);
---
+++ Day34/index.ts
@@ -0,0 +1,19 @@
+//*Question 100:* Use the JavaScript Math object to find the square root of 144.
+
+const squareRoot : number = Math.sqrt(81)
+
+console.log(squareRoot);
+
+//*Question 101:* Generate a random integer between 1 and 10.
+
+function randomInteger():number{
+      return Math.floor(Math.random()*10) + 1
+}
+
+console.log(randomInteger());
+
+//*Question 102:* Calculate and log the absolute difference between the number -5 and 5.
+
+const difference : number = Math.abs(-6 - 5)
+
+console.log(difference);
