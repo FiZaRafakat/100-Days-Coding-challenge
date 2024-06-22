c67704c5bef0599047fe2f7443c63deb06e5eab5
FiZaRafakat <164252341+FiZaRafakat@users.noreply.github.com>
2024-06-21 22:35:46 -0700

    Coding Challenge 7

---
+++ Day41/index.js
@@ -0,0 +1,32 @@
+"use strict";
+//*Question 121:* Implement a for loop that counts from 1 to 10 but skips number 5 using the continue statement.
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+    console.log(i);
+}
+console.log("\n***********************************\n");
+//*Question 122:* Use a while loop to count down from 10 to 1 and breaks the loop when it reaches 5.
+let count = 10;
+while (count > 0) {
+    if (count === 5) {
+        break;
+    }
+    console.log(count);
+    count--;
+}
+console.log("\n***********************************\n");
+//*Question 123:* Create a loop that iterates through a string and stops when it finds the first vowel.
+function firstVowel(words) {
+    const vowels = "aeiouAEIOU";
+    for (const char of words) {
+        if (vowels.includes(char)) {
+            console.log(`First Vowel Found : ${char}`);
+            break;
+        }
+        console.log(char);
+    }
+}
+firstVowel('Fiza Rafakat');
+console.log("\n***********************************\n");
